8bcc3ef73df49f7545cbce0d14dfa629935f54bf
ether1313 <topnroyal.13@gmail.com>
2025-12-08 21:06:17 +0800

    update something

--- components/FloatingIcon.tsx
+++ components/FloatingIcon.tsx
@@ -26,7 +26,7 @@ export default function FloatingIcon() {
         href="https://www.gucci9.vip/"
         target="_blank"
         rel="noopener noreferrer"
-        className="rounded-full transition-transform duration-200 hover:scale-110"
+        className="transition-transform duration-200 hover:scale-110"
       >
         <Image
           src="/playnow-floatingicon.gif"
